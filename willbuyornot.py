009dea1794ee599297102073ee41bacc3106126b
Tarcisio Ferraz <tarcisio@MacBook-Pro-de-Tarcisio.local>
2019-06-04 19:36:52 -0300

    Usando train_test_split para separar os dados de treino e testes

--- willbuyornot.py
+++ willbuyornot.py
@@ -1,5 +1,6 @@
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 uri='https://gist.githubusercontent.com/guilhermesilveira/2d2efa37d66b6c84a722ea627a897ced/raw/10968b997d885cbded1c92938c7a9912ba41c615/tracking.csv'
@@ -14,6 +15,8 @@ mapa = {
     "bought": "comprou"
 }
 
+SEED = 20
+
 dados = dados.rename(columns = mapa)
 
 # separando as colunas referentes a x e y 
@@ -21,11 +24,7 @@ dados_x = dados[["principal", "como_funciona", "contato"]]
 dados_y = dados["comprou"]
 
 # separando dados de treino e testes
-treino_x = dados_x[:75]
-treino_y = dados_y[:75]
-
-teste_x = dados_x[75:]
-teste_y = dados_y[75:]
+treino_x, teste_x, treino_y, teste_y = train_test_split(dados_x, dados_y, test_size = 0.25, random_state = SEED, stratify = dados_y)
 
 print("Treinaremos com {} elementos e testaremos com {} elementos.".format(len(treino_y), len(teste_y)))
 
